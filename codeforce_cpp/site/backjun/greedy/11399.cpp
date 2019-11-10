26c94f40039a5607f017e1d042e2338b2c70b092
yu-young-hoon <dbtlek@gmail.com>
2019-11-11 02:33:49 +0900

    - 백준 greedy 11399

---
+++ codeforce_cpp/site/backjun/greedy/11399.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+#ifndef NDEBUG
+	ifstream in("in.txt");
+	streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
+#endif
+    ll c, answer = 0, reduce = 0;
+    multiset<ll> p;
+    cin >> c;
+    for (ll i = 0 ; i < c ; ++i) {
+        ll bankTime;
+        cin >> bankTime;
+        p.insert(bankTime);
+    }
+
+    multiset<ll>::iterator itr = p.begin();
+    for (itr ; itr != p.end() ; ++itr) {
+        reduce += *itr;
+        answer += reduce;
+    }
+    
+    cout << answer;
+
+	return 0;
+}
---
+++ codeforce_cpp/site/backjun/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+#ifndef NDEBUG
+	ifstream in("in.txt");
+	streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
+#endif
+
+	return 0;
+}
