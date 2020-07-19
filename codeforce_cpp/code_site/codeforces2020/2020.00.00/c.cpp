6f9937dd9710f091886cc8015c74f4e8a1cd83c9
yh-mac <dbtlek@gmail.com>
2020-07-19 23:12:27 +0900

    -2020 코드포스 템플릿

---
+++ codeforce_cpp/code_site/codeforces2020/2020.00.00/c.cpp
@@ -0,0 +1,19 @@
+#ifdef WIN32 
+#include "..\\..\\..\\stdc++.h"
+#elif __APPLE__
+#include "../../../stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+#ifdef YH
+	ifstream in("in.txt");
+	streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
+#endif
+	return 0;
+}
