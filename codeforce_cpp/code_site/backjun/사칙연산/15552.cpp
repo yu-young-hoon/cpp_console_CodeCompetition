0de2a40cfc6ec2531677892ef36b408d89c15be3
yh-mac <dbtlek@gmail.com>
2020-07-20 23:27:00 +0900

    -백준 사칙연산 빠른처리

---
+++ codeforce_cpp/code_site/backjun/사칙연산/15552.cpp
@@ -0,0 +1,29 @@
+#ifdef WIN32 
+#include "..\\..\\..\\stdc++.h"
+#elif __APPLE__
+#include "../../../stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+#ifdef YH
+	ifstream in("in.txt");
+	streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
+#endif
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    ll cnt;
+    cin >> cnt;
+
+    for (ll i = 0 ; i < cnt ; ++i) {
+        ll a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+	return 0;
+}
